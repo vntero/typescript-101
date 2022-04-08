033ef6f3ca6baaf2603f8902a72b4c98826671c2
vntero <shin.antero@gmail.com>
2022-04-08 19:23:24 +0200

    started with object types

--- app.ts
+++ app.ts
@@ -1,14 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean) {
-    if(showResult) {
-        console.log(n1 + n2)
-    } else {
-        return n1 + n2
-    }
-    
+const person = {
+    name: 'Itachi',
+    age: 31
 }
 
-const number1 = 5
-const number2 = 2.8
-const printResult = true
-
-add(number1, number2, printResult)
+console.log(person.nickname)
