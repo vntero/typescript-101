b44815a572089de0104d45bb39cac249bc76fa2f
vntero <shin.antero@gmail.com>
2022-04-11 12:51:53 +0200

    callbacks in TS

--- app.ts
+++ app.ts
@@ -6,4 +6,16 @@ function printResult(num: number): void {
     console.log('Result: ' + num)
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2
+    cb(result)
+}
+
 printResult(add(5, 12))
+
+let combineValues: (a: number, b: number) => number
+
+combineValues = add
+// combineValues = printResult
+
+console.log(combineValues(8, 8))
