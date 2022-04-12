e7fda16b4e97e86dc18d81cf3fe5e2adf5904972
vntero <shin.antero@gmail.com>
2022-04-12 09:49:41 +0200

    shorthand initialisation

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,34 @@
 class Department {
     name: string
+    private employees: string[] = []
 
     constructor(n: string) {
         this.name = n
     }
 
-    describe() {
+    describe(this: Department) {
         console.log('Department: ' + this.name)
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
     }
 }
 
 const accounting = new Department('Accounting')
-
+accounting.addEmployee('Yuna')
+accounting.addEmployee('Eurico')
+// THIS SHOWS AN ERROR NOW BECAUSE employee is set with a PRIVATE MODIFIER
+// accounting.employees[2] = 'Nika'
+accounting.addEmployee('Domingos')
 console.log(accounting)
-
 accounting.describe()
+accounting.printEmployeeInformation()
+
+// const accountingCopy = { name: 'Sasuke', describe: accounting.describe }
+// accountingCopy.describe()
